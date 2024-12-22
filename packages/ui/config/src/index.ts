71fd246bb860615cbacdb68e474efbe12c27b691
herokwon <contact@herokwon.dev>
2024-12-22 23:39:06 +0900

    Feat (ui-config): TailwindCSS 프리셋 구현

--- packages/ui/config/src/index.ts
+++ packages/ui/config/src/index.ts
@@ -1,2 +1,3 @@
+export { default as tailwindPresets } from '@repo/tailwind-config';
 export { default as tailwindPlugin } from './plugin';
 export { default as tailwindExtendedTheme } from './theme';
