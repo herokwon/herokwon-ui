e3f27d33ff90759bc178f5177f94aab0d2126da0
herokwon <contact@herokwon.dev>
2024-12-23 18:13:30 +0900

    Refactor (ui-config): TailwindCSS 커스텀 유틸리티 스타일 객체화

--- packages/ui/config/src/plugin.ts
+++ packages/ui/config/src/plugin.ts
@@ -6,7 +6,11 @@ import {
   baseHeading,
   baseInput,
   baseTable,
+  bgUnderline,
   customVariants,
+  hiddenScrollbar,
+  scrollbarX,
+  scrollbarY,
 } from './plugins';
 
 const tailwindPlugin = plugin(
@@ -27,141 +31,18 @@ const tailwindPlugin = plugin(
     });
 
     matchUtilities({
-      'bg-underline': value => ({
-        'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-        'background-size': `0% ${value}`,
-        'background-repeat': 'no-repeat',
-        'background-position': '0 100%',
-        transition: 'background-size 150ms linear',
-        '&:hover': {
-          'background-size': `100% ${value}`,
-        },
-        '&.active': {
-          'background-size': `100% ${value}`,
-        },
-      }),
-      'group-bg-underline': value => ({
-        'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-        'background-size': `0% ${value}`,
-        'background-repeat': 'no-repeat',
-        'background-position': '0 100%',
-        transition: 'background-size 150ms linear',
-        '.group:hover &': {
-          'background-size': `100% ${value}`,
-        },
-        '&.active': {
-          'background-size': `100% ${value}`,
-        },
-      }),
-      'peek-bg-underline': value => ({
-        'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-        'background-size': `0% ${value}`,
-        'background-repeat': 'no-repeat',
-        'background-position': '0 100%',
-        transition: 'background-size 150ms linear',
-        '.peek:hover ~ &': {
-          'background-size': `100% ${value}`,
-        },
-        '&.active': {
-          'background-size': `100% ${value}`,
-        },
-      }),
+      'bg-underline': value => bgUnderline.match.default(value),
+      'group-bg-underline': value => bgUnderline.match.group(value),
+      'peek-bg-underline': value => bgUnderline.match.peek(value),
     });
 
     addUtilities({
-      ...Object.fromEntries(
-        Array.from({ length: 30 }, (_, i) => {
-          const index =
-            i <= 12
-              ? i
-              : i <= 14
-                ? 12 + 2 * (i - 12)
-                : i <= 26
-                  ? 16 + 4 * (i - 14)
-                  : i <= 28
-                    ? 64 + 8 * (i - 28)
-                    : 96;
-
-          return [
-            `.bg-underline-${index}`,
-            {
-              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              'background-repeat': 'no-repeat',
-              'background-position': '0 100%',
-              transition: 'background-size 150ms linear',
-              '&:hover': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-              '&.active': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-            },
-          ];
-        }),
-      ),
-      ...Object.fromEntries(
-        Array.from({ length: 30 }, (_, i) => {
-          const index =
-            i <= 12
-              ? i
-              : i <= 14
-                ? 12 + 2 * (i - 12)
-                : i <= 26
-                  ? 16 + 4 * (i - 14)
-                  : i <= 28
-                    ? 64 + 8 * (i - 28)
-                    : 96;
-
-          return [
-            `.group-bg-underline-${index}`,
-            {
-              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              'background-repeat': 'no-repeat',
-              'background-position': '0 100%',
-              transition: 'background-size 150ms linear',
-              '.group:hover &': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-              '&.active': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-            },
-          ];
-        }),
-      ),
-      ...Object.fromEntries(
-        Array.from({ length: 30 }, (_, i) => {
-          const index =
-            i <= 12
-              ? i
-              : i <= 14
-                ? 12 + 2 * (i - 12)
-                : i <= 26
-                  ? 16 + 4 * (i - 14)
-                  : i <= 28
-                    ? 64 + 8 * (i - 28)
-                    : 96;
-
-          return [
-            `.peek-bg-underline-${index}`,
-            {
-              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
-              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              'background-repeat': 'no-repeat',
-              'background-position': '0 100%',
-              transition: 'background-size 150ms linear',
-              '.peek:hover ~ &': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-              '&.active': {
-                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
-              },
-            },
-          ];
-        }),
-      ),
+      '.scrollbar-x': scrollbarX,
+      '.scrollbar-y': scrollbarY,
+      '.hidden-scrollbar': hiddenScrollbar,
+      ...bgUnderline.add.default,
+      ...bgUnderline.add.group,
+      ...bgUnderline.add.peek,
     });
   },
 );
