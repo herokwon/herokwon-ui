4307ef923e1f1499e2037839b0dfc01d1d13c2c2
HeroKwon <contact@herokwon.dev>
2024-12-23 18:17:57 +0900

    Update: `@herokwon/ui-config@0.2.0` (#14)
    * Feat (ui-config): TailwindCSS 커스텀 변수(xs, scrollbar, slider, progress) 추가
    
    * Refactor (ui-config): TailwindCSS 커스텀 유틸리티 스타일 객체화
    
    * Test (ui-config): TailwindCSS 커스텀 유틸리티 스타일 객체 테스트 코드 추가

--- packages/ui/config/src/__tests__/plugin.test.ts
+++ packages/ui/config/src/__tests__/plugin.test.ts
@@ -1,8 +1,24 @@
-import { customVariants } from '../plugins';
+import {
+  bgUnderline,
+  customVariants,
+  hiddenScrollbar,
+  scrollbarX,
+  scrollbarY,
+} from '../plugins';
 
-describe('TailwindCSS Custom Plugin', () => {
+describe('tailwindPlugin', () => {
   it('customVariants', () => {
     expect(customVariants).toStrictEqual({
+      scrollbar: '&::-webkit-scrollbar',
+      'scrollbar-thumb': '&::-webkit-scrollbar-thumb',
+      'scrollbar-track': '&::-webkit-scrollbar-track',
+      'slider-thumb': ['&::-webkit-slider-thumb', '&::-moz-range-thumb'],
+      'slider-track': [
+        '&::-webkit-slider-runnable-track',
+        '&::-moz-range-track',
+      ],
+      'progress-bar': '&::-webkit-progress-bar',
+      'progress-value': '&::-webkit-progress-value',
       'not-open': '&:not([open])',
       'not-disabled': '&:not(:disabled)',
       'not-hover': '&:not(:hover)',
@@ -11,5 +27,198 @@ describe('TailwindCSS Custom Plugin', () => {
       xs: '@media (min-width: 512px)',
       'max-xs': '@media not all and (min-width: 512px)',
     });
+  });
+
+  it('scrollbarX', () => {
+    expect(scrollbarX).toHaveProperty('overflow-x', 'auto');
+    expect(scrollbarX['&::-webkit-scrollbar']).toStrictEqual({
+      width: '100%',
+      height: '7px',
+    });
+    expect(scrollbarX['&::-webkit-scrollbar-thumb']).toStrictEqual({
+      cursor: 'pointer',
+      'border-radius': '9999px',
+      'background-color': '#3B82F6',
+    });
+    expect(scrollbarX['.dark &::-webkit-scrollbar-thumb']).toStrictEqual({
+      'background-color': '#2563EB',
+    });
+    expect(scrollbarX['&::-webkit-scrollbar-track']).toStrictEqual({
+      cursor: 'pointer',
+      'background-color': 'transparent',
+    });
+  });
+
+  it('scrollbarY', () => {
+    expect(scrollbarY).toHaveProperty('overflow-y', 'auto');
+    expect(scrollbarY['&::-webkit-scrollbar']).toStrictEqual({
+      width: '7px',
+      height: '100%',
+    });
+    expect(scrollbarY['&::-webkit-scrollbar-thumb']).toStrictEqual({
+      cursor: 'pointer',
+      'border-radius': '9999px',
+      'background-color': '#3B82F6',
+    });
+    expect(scrollbarY['.dark &::-webkit-scrollbar-thumb']).toStrictEqual({
+      'background-color': '#2563EB',
+    });
+    expect(scrollbarY['&::-webkit-scrollbar-track']).toStrictEqual({
+      cursor: 'pointer',
+      'background-color': 'transparent',
+    });
+  });
+
+  it('hiddenScrollbar', () => {
+    expect(hiddenScrollbar).toHaveProperty('-ms-overflow-style', 'none');
+    expect(hiddenScrollbar['&::-webkit-scrollbar']).toStrictEqual({
+      display: 'none',
+    });
+  });
+
+  it('bg-underline in matchUtilities', () => {
+    const value = '1px;';
+
+    expect(bgUnderline.match.default(value)).toStrictEqual({
+      'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+      'background-size': `0% ${value}`,
+      'background-repeat': 'no-repeat',
+      'background-position': '0 100%',
+      transition: 'background-size 150ms linear',
+      '&:hover': {
+        'background-size': `100% ${value}`,
+      },
+      '&.active': {
+        'background-size': `100% ${value}`,
+      },
+    });
+    expect(bgUnderline.match.group(value)).toStrictEqual({
+      'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+      'background-size': `0% ${value}`,
+      'background-repeat': 'no-repeat',
+      'background-position': '0 100%',
+      transition: 'background-size 150ms linear',
+      '.group:hover &': {
+        'background-size': `100% ${value}`,
+      },
+      '&.active': {
+        'background-size': `100% ${value}`,
+      },
+    });
+    expect(bgUnderline.match.peek(value)).toStrictEqual({
+      'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+      'background-size': `0% ${value}`,
+      'background-repeat': 'no-repeat',
+      'background-position': '0 100%',
+      transition: 'background-size 150ms linear',
+      '.peek:hover ~ &': {
+        'background-size': `100% ${value}`,
+      },
+      '&.active': {
+        'background-size': `100% ${value}`,
+      },
+    });
+  });
+
+  it('bg-underline in addUtilities', () => {
+    expect(bgUnderline.add.default).toStrictEqual(
+      Object.fromEntries(
+        Array.from({ length: 30 }, (_, i) => {
+          const index =
+            i <= 12
+              ? i
+              : i <= 14
+                ? 12 + 2 * (i - 12)
+                : i <= 26
+                  ? 16 + 4 * (i - 14)
+                  : i <= 28
+                    ? 64 + 8 * (i - 28)
+                    : 96;
+
+          return [
+            `.bg-underline-${index}`,
+            {
+              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              'background-repeat': 'no-repeat',
+              'background-position': '0 100%',
+              transition: 'background-size 150ms linear',
+              '&:hover': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+              '&.active': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+            },
+          ];
+        }),
+      ),
+    );
+    expect(bgUnderline.add.group).toStrictEqual(
+      Object.fromEntries(
+        Array.from({ length: 30 }, (_, i) => {
+          const index =
+            i <= 12
+              ? i
+              : i <= 14
+                ? 12 + 2 * (i - 12)
+                : i <= 26
+                  ? 16 + 4 * (i - 14)
+                  : i <= 28
+                    ? 64 + 8 * (i - 28)
+                    : 96;
+
+          return [
+            `.group-bg-underline-${index}`,
+            {
+              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              'background-repeat': 'no-repeat',
+              'background-position': '0 100%',
+              transition: 'background-size 150ms linear',
+              '.group:hover &': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+              '&.active': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+            },
+          ];
+        }),
+      ),
+    );
+    expect(bgUnderline.add.peek).toStrictEqual(
+      Object.fromEntries(
+        Array.from({ length: 30 }, (_, i) => {
+          const index =
+            i <= 12
+              ? i
+              : i <= 14
+                ? 12 + 2 * (i - 12)
+                : i <= 26
+                  ? 16 + 4 * (i - 14)
+                  : i <= 28
+                    ? 64 + 8 * (i - 28)
+                    : 96;
+
+          return [
+            `.peek-bg-underline-${index}`,
+            {
+              'background-image': `linear-gradient(to right, var(--tw-gradient-stops))`,
+              'background-size': `0% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              'background-repeat': 'no-repeat',
+              'background-position': '0 100%',
+              transition: 'background-size 150ms linear',
+              '.peek:hover ~ &': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+              '&.active': {
+                'background-size': `100% ${index === 0 ? '0px' : `${index / 4}rem`}`,
+              },
+            },
+          ];
+        }),
+      ),
+    );
   });
 });
--- packages/ui/config/src/plugins/index.ts
+++ packages/ui/config/src/plugins/index.ts
@@ -3,18 +3,4 @@ export * from './code';
 export * from './heading';
 export * from './input';
 export * from './table';
-
-export const customVariants: { [key: string]: string | string[] } = {
-  // not-()
-  'not-open': '&:not([open])',
-  'not-disabled': '&:not(:disabled)',
-  'not-hover': '&:not(:hover)',
-
-  // group-() & peer-()
-  'group-not-disabled': '.group:not(:disabled) &',
-  'peer-not-disabled': '.peer:not(:disabled) ~ &',
-
-  // size
-  xs: '@media (min-width: 512px)',
-  'max-xs': '@media not all and (min-width: 512px)',
-};
+export * from './utils';
