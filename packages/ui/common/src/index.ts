bbece56f2eba340101e270ae188325a5c42c7c99
herokwon <contact@herokwon.dev>
2024-12-29 22:25:51 +0900

    Refactor (ui-common): `Container`, `Icon` 컴포넌트 내보내기 추가

--- packages/ui/common/src/index.ts
+++ packages/ui/common/src/index.ts
@@ -3,5 +3,7 @@ import './index.css';
 export * from './types';
 
 export { default as Box } from './Box';
+export { default as Container } from './Container';
 export { default as Flex } from './Flex';
 export { default as Grid } from './Grid';
+export { default as Icon } from './Icon';
