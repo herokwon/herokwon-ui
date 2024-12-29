0f6e376d413cbeaa92ec086ac1f3bfb9a70f83ec
herokwon <contact@herokwon.dev>
2024-12-29 22:19:02 +0900

    Refactor (ui-common): 테스트 코드 활용을 위한 상수 내보내기 추가

--- packages/ui/common/src/types.ts
+++ packages/ui/common/src/types.ts
@@ -1,8 +1,12 @@
 import type { Globals } from 'csstype';
 
 export const ELEMENT_SIZES = ['xs', 'sm', 'md', 'lg', 'xl'] as const;
-const ELEMENT_SPACINGS = [0, 2, 4, 6, 8, 10, 12, 16, 20] as const;
-const ELEMENT_STATUS = ['isDisabled', 'isSelected', 'isLoading'] as const;
+export const ELEMENT_SPACINGS = [0, 2, 4, 6, 8, 10, 12, 16, 20] as const;
+export const ELEMENT_STATUS = [
+  'isDisabled',
+  'isSelected',
+  'isLoading',
+] as const;
 
 // Element
 export type ElementSize = (typeof ELEMENT_SIZES)[number];
