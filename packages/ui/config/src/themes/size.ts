4d96ae91ea7de66ec7cefbe999b60b98637b4ff0
herokwon <contact@herokwon.dev>
2025-02-20 22:50:58 +0900

    Remove (config): size 관련 속성 파일 제거 (feat. `TailwindCSS` v4 migration)

--- packages/ui/config/src/themes/size.ts
+++
@@ -1,6 +0,0 @@
-const extendedSizes = Object.fromEntries(
-  Array.from({ length: 2001 }, (_, i) => [i, `${i}px`]),
-);
-
-export const extendedWidth = extendedSizes;
-export const extendedHeight = extendedSizes;
