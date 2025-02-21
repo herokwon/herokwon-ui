79b1dacad63817b95d591a7ff0afc3e0e717c085
herokwon <contact@herokwon.dev>
2025-02-21 13:53:12 +0900

    Test (ui-config): `TailwindCSS` 커스텀 확장 테마 size 관련 속성 테스트 코드 제거

--- packages/ui/config/src/__tests__/theme.test.ts
+++ packages/ui/config/src/__tests__/theme.test.ts
@@ -2,14 +2,11 @@ import tailwindExtendedTheme from '../theme';
 import { extendedBgColors } from '../themes/background';
 import { extendedColors } from '../themes/color';
 import { extendedOpacity } from '../themes/effect';
-import { extendedHeight, extendedWidth } from '../themes/size';
 import { extendedTextColors } from '../themes/typography';
 
 describe('tailwindExtendedTheme', () => {
   it('should have entire properties', () => {
     expect(tailwindExtendedTheme).toStrictEqual({
-      width: extendedWidth,
-      height: extendedHeight,
       colors: extendedColors,
       textColor: extendedTextColors,
       backgroundColor: extendedBgColors,
