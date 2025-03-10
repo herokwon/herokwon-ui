8f69998e2905f21c100e3464e7b0c91166536314
herokwon <contact@herokwon.dev>
2025-03-10 14:20:49 +0900

    Remove (tailwind-config): TailwindCSS 환경설정 파일 제거

--- packages/ui/config/src/index.ts
+++ packages/ui/config/src/index.ts
@@ -1,3 +1,2 @@
-export { default as tailwindPreset } from '@repo/tailwind-config';
 export { default as tailwindPlugin } from './plugin';
 export { default as tailwindExtendedTheme } from './theme';
