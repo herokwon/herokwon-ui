42d714d856b19aa29eb7ae8f8728de6e9a5fc1c8
herokwon <contact@herokwon.dev>
2025-03-11 22:28:05 +0900

    Chore (ui-common): TailwindCSS 관련 의존성 제거

--- packages/ui/common/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-import postcssConfig from '@repo/tailwind-config/postcss';
-
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  ...postcssConfig,
-};
-
-export default config;
