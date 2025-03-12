e2cd71b3652496bfbf1b1f4a760dcf6eb55e9a59
HeroKwon <contact@herokwon.dev>
2025-03-12 17:30:53 +0900

    Refactor: TailwindCSS preflight (feat. `@herokwon/ui@0.2.0`) (#94)
    * Remove (tailwind-config): TailwindCSS 환경설정 파일 제거
    
    * Feat (tailwind-config): `theme.css` 파일 생성
    
    * Refactor (ui): TailwindCSS 커스텀 확장 테마 마이그레이션
    
    * Remove (ui-config): TailwindCSS 커스텀 확장 테마 제거 (feat. 마이그레이션 완료)
    
    * Feat (tailwind-config): `base.css` 파일 생성
    
    * Refactor (ui): TailwindCSS 커스텀 플러그인 마이그레이션
    
    * Remove (ui-config): TailwindCSS 커스텀 플러그인 제거 (feat. 마이그레이션 완료)
    
    * Chore (ui-config): `@herokwon/ui-config` 배포 중단
    
    * Chore (ui-common): TailwindCSS 관련 의존성 제거
    
    * Refactor (ui-common): 스타일시트 파일 일원화
    
    * Chore (ui): `@herokwon/ui-common` 의존성 제거
    
    * Docs (ui): 0.2.0 버전 업데이트 문서 자동 생성 (by. `changeset`)
    
    * Fix (ui): 스타일 속성명 오타 수정 (`maxwidth` → `max-width`)
    
    * Chore (storybook): 스타일 시트 참조 경로 변경
    
    * Design (storybook): 유틸리티 클리스명 수정 (feat. TailwindCSS preflight 마이그레이션)

--- packages/ui/config/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as tailwindPreset } from '@repo/tailwind-config';
-export { default as tailwindPlugin } from './plugin';
-export { default as tailwindExtendedTheme } from './theme';
