be25f2795831144673db4e4e078a144f14941676
herokwon <contact@herokwon.dev>
2025-03-19 11:24:58 +0900

    Chore (ui): `vitest` coverage 환경설정 추가 구성

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -5,11 +5,14 @@ export default defineConfig({
   plugins: [react()],
   test: {
     setupFiles: './vitest.setup',
+    include: ['**/__tests__/**/*.(spec|test).ts?(x)'],
     coverage: {
       provider: 'v8',
+      enabled: true,
+      include: ['**/__tests__'],
+      exclude: ['**/!(*.spec|*.test).ts?(x)'],
     },
     pool: 'threads',
-    include: ['./**/__tests__/**/*.(spec|test).ts?(x)'],
     environment: 'jsdom',
     globals: true,
   },
