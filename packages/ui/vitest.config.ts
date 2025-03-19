9398bc0cac4ad6374f950974d4dc0443db473772
HeroKwon <contact@herokwon.dev>
2025-03-19 11:31:49 +0900

    Chore: `Vitest` coverage configuration (feat. v8) (#102)
    * Rename (ui): `vitest-setup.ts` 파일명 변경 → `vitest.setup.ts`
    
    * Chore (ui): `@vitest/coverage-v8` 의존성 추가 설정
    
    * Chore (ui): `vitest` coverage 환경설정 추가 구성

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
   test: {
-    setupFiles: './vitest-setup',
+    setupFiles: './vitest.setup',
+    include: ['**/__tests__/**/*.(spec|test).ts?(x)'],
     coverage: {
       provider: 'v8',
+      enabled: true,
+      include: ['**/__tests__'],
+      exclude: ['**/!(*.spec|*.test).ts?(x)'],
     },
     pool: 'threads',
-    include: ['./**/__tests__/**/*.(spec|test).ts?(x)'],
     environment: 'jsdom',
     globals: true,
   },
